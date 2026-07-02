c91575bbdea47ff79e922405fc109b5398b1822a
mahajanatharv2009-design <242420664+mahajanatharv2009-design@users.noreply.github.com>
2026-07-02 08:29:35 +0000

    Update portfolio projects from GitHub

--- generated-projects.js
+++ generated-projects.js
@@ -1,1 +1,62 @@
-window.GITHUB_PROJECTS = [];
+window.GITHUB_PROJECTS = [
+  {
+    "title": "time helper",
+    "description": "A vertule pet with timer that helps you to manage time",
+    "repo": "timy-wimey",
+    "owner": "Atharvamaj",
+    "url": "https://github.com/Atharvamaj/timy-wimey",
+    "homepage": "",
+    "website": "",
+    "updatedAt": "2026-05-07T16:56:10Z"
+  },
+  {
+    "title": "happy-new-year",
+    "description": "happy new year prank card.",
+    "repo": "happy-new-year",
+    "owner": "Atharvamaj",
+    "url": "https://github.com/Atharvamaj/happy-new-year",
+    "homepage": "",
+    "website": "",
+    "updatedAt": "2026-01-01T03:52:56Z"
+  },
+  {
+    "title": "VOCA",
+    "description": "Voca removes friction, reduces overwhelm, and helps ideas become real. </p",
+    "repo": "VOCA",
+    "owner": "Atharvamaj",
+    "url": "https://github.com/Atharvamaj/VOCA",
+    "homepage": "https://voca-blush.vercel.app",
+    "website": "https://voca-blush.vercel.app",
+    "updatedAt": "2025-12-06T23:00:08Z"
+  },
+  {
+    "title": "VEXRoboticsCodeteam-11230B",
+    "description": "Team Name - Under Dogs",
+    "repo": "VEX_Robotics_Code_team-11230B",
+    "owner": "Atharvamaj",
+    "url": "https://github.com/Atharvamaj/VEX_Robotics_Code_team-11230B",
+    "homepage": "",
+    "website": "",
+    "updatedAt": "2025-11-27T20:42:24Z"
+  },
+  {
+    "title": "JarviousAIAssistant",
+    "description": "A local based assistant for windows os",
+    "repo": "J.A.R.V.I.S.",
+    "owner": "mahajanatharv2009-design",
+    "url": "https://github.com/mahajanatharv2009-design/J.A.R.V.I.S.",
+    "homepage": "",
+    "website": "",
+    "updatedAt": "2025-11-07T00:21:34Z"
+  },
+  {
+    "title": "Midswim Surgical Simulation",
+    "description": "rember to install theas :",
+    "repo": "Midswim-Surgical-Simulation",
+    "owner": "Atharvamaj",
+    "url": "https://github.com/Atharvamaj/Midswim-Surgical-Simulation",
+    "homepage": "",
+    "website": "",
+    "updatedAt": "2024-12-14T22:28:54Z"
+  }
+];
