24db2cb71a080904ce652bd2ef09e3fc5a593f59
mahajanatharv2009-design <242420664+mahajanatharv2009-design@users.noreply.github.com>
2026-07-07 00:09:52 +0000

    GitHub project bot manual update

--- generated-projects.js
+++ generated-projects.js
@@ -1,1 +1,98 @@
-window.GITHUB_PROJECTS = [];
+window.GITHUB_PROJECTS = [
+  {
+    "title": "Resume AtharvMahajan",
+    "description": "https://mahajanatharv2009-design.github.io/ResumeAtharvMahajan/ link to portfolio",
+    "category": "GitHub",
+    "pin": null,
+    "repo": "Resume_AtharvMahajan",
+    "owner": "mahajanatharv2009-design",
+    "url": "https://github.com/mahajanatharv2009-design/Resume_AtharvMahajan",
+    "homepage": "https://mahajanatharv2009-design.github.io/Resume_AtharvMahajan/",
+    "website": "https://mahajanatharv2009-design.github.io/Resume_AtharvMahajan/",
+    "updatedAt": "2026-07-07T00:09:18Z"
+  },
+  {
+    "title": "Bday-card",
+    "description": "Check out who I wished a Happy Birthday to last!!!",
+    "category": "Web",
+    "pin": null,
+    "repo": "Bday-card",
+    "owner": "mahajanatharv2009-design",
+    "url": "https://github.com/mahajanatharv2009-design/Bday-card",
+    "homepage": "https://mahajanatharv2009-design.github.io/Bday-card/",
+    "website": "https://mahajanatharv2009-design.github.io/Bday-card/",
+    "updatedAt": "2026-07-06T23:53:57Z"
+  },
+  {
+    "title": "J.A.R.V.I.S.",
+    "description": "A local based assistant for windows os",
+    "category": "Assistant",
+    "pin": null,
+    "repo": "J.A.R.V.I.S.",
+    "owner": "mahajanatharv2009-design",
+    "url": "https://github.com/mahajanatharv2009-design/J.A.R.V.I.S.",
+    "homepage": "",
+    "website": "",
+    "updatedAt": "2026-07-06T23:52:46Z"
+  },
+  {
+    "title": "VOCA",
+    "description": "An accessibility focused web application that uses speech to text and AI gen artwork to help users quickly capture, organize, and visualize ideas.",
+    "category": "Web",
+    "pin": null,
+    "repo": "VOCA",
+    "owner": "Atharvamaj",
+    "url": "https://github.com/Atharvamaj/VOCA",
+    "homepage": "https://voca-blush.vercel.app",
+    "website": "https://voca-blush.vercel.app",
+    "updatedAt": "2026-07-06T23:46:28Z"
+  },
+  {
+    "title": "Happy New Year Prank Card",
+    "description": "This is a simple webpage that looks like a New Year's greeting. When opened, it preloads a hidden Rick Astley video.",
+    "category": "Web",
+    "pin": null,
+    "repo": "happy-new-year",
+    "owner": "Atharvamaj",
+    "url": "https://github.com/Atharvamaj/happy-new-year",
+    "homepage": "https://github.com/Atharvamaj/happy-new-year",
+    "website": "https://github.com/Atharvamaj/happy-new-year",
+    "updatedAt": "2026-07-06T23:43:07Z"
+  },
+  {
+    "title": "VEXRobotics",
+    "description": "This will be our Teams up comming websit",
+    "category": "Web",
+    "pin": null,
+    "repo": "VEX_Robotics_Code_team-11230B",
+    "owner": "Atharvamaj",
+    "url": "https://github.com/Atharvamaj/VEX_Robotics_Code_team-11230B",
+    "homepage": "",
+    "website": "",
+    "updatedAt": "2026-07-06T23:42:26Z"
+  },
+  {
+    "title": "Timy Wimey",
+    "description": "A virtual pet with a timer that helps you manage time.",
+    "category": "Web",
+    "pin": null,
+    "repo": "timy-wimey",
+    "owner": "Atharvamaj",
+    "url": "https://github.com/Atharvamaj/timy-wimey",
+    "homepage": "",
+    "website": "",
+    "updatedAt": "2026-07-06T23:41:50Z"
+  },
+  {
+    "title": "Midswim Surgical Simulation",
+    "description": "rember to install theas :",
+    "category": "GitHub",
+    "pin": null,
+    "repo": "Midswim-Surgical-Simulation",
+    "owner": "Atharvamaj",
+    "url": "https://github.com/Atharvamaj/Midswim-Surgical-Simulation",
+    "homepage": "",
+    "website": "",
+    "updatedAt": "2024-12-14T22:28:54Z"
+  }
+];
